0b2f8f04eba7fae5cb0726230c2abacc62d49ccd
SlaxX <schneja@web.de>
2017-10-06 18:05:40 +0200

    added javadoc

--- src/main/java/de/slxSoft/graphGen/Generator.java
+++ src/main/java/de/slxSoft/graphGen/Generator.java
@@ -1,7 +1,11 @@
 package de.slxSoft.graphGen;
 
 /**
- * Created by SlaxX on 05.10.2017.
+ * Generates a random curve revolving around a predefined function and returns it as an integer array.
+ * for the setup, look up the javadoc of the setter methods of the Graph.
+ *
+ * Later, this Generator will Manage multiple Graphs
+ * and probably have more cool functions :P
  */
 public class Generator {
 
@@ -11,6 +15,12 @@ public class Generator {
         graph = new Graph();
     }
 
+    /**
+     * Generates values with previously set-up variables in the Graph
+     * @return Returns the Array of generated Values
+     * For later Versions, that can generate multiple Graphs at once, use this generate() method
+     * As of now, you may also use the generate() method directly from the Graph
+     */
     public int[] generate(){
         return graph.generate();
     }
--- src/main/java/de/slxSoft/graphGen/Graph.java
+++ src/main/java/de/slxSoft/graphGen/Graph.java
@@ -17,26 +17,67 @@ public class Graph {
     private String function = "50+3.5*x";
     private int[] values;
 
+    /**
+     * DEFAULT is 0.4 (40%)
+     * @param offset Maximum offset from the Function in percent
+     */
     public void setMaxOffset(int offset) {
         maxOffset = offset;
     }
 
+    /**
+     * DEFAULT is 100
+     * @param size Size of the Array
+     * Always starts at 0 and includes the maximum value (therefore contains size+1 values)
+     */
     public void setSize(int size) {
         this.size = size;
     }
 
+    /**
+     * DEFAULT is "50 + 3.5 * x" --> with default size a function that ranges from 50 - 400
+     * @param function A String that contains the Function that the Graph will determine values after
+     * "x" is a predefined variable, other variables are not allowed
+     * for more info on what constants and operators are usable, visit http://www.objecthunter.net/exp4j
+     */
     public void setFunction(String function) {
         this.function = function;
     }
 
+    /**
+     * DEFAULT is 0
+     * @param speed ChangeSpeed defines the chance for the curve to change it's value
+     * the higher the value, the less the curve will have "noise"
+     * (but 0 seems to be the best..)
+     */
     public void setChangeSpeed(int speed) {
         changeSpeed = speed * 2 + 1;
     }
 
+    /**
+     * DEFAULT is 10
+     * To further reduce noise, the curve will be on a "cooldown" after a big value change
+     * @param speed CooldownSpeed sets the speed of the cooldown regeneration
+     * the lower the value, the slower the cooldown will regenerate
+     */
     public void setCooldownSpeed(int speed) {
         cooldownSpeed = speed;
     }
 
+    /**
+     *
+     * @return Returns the Array of generated Values
+     * If Generator.generate() wasn't executed yet, this will be empty
+     * Alternatively, Generator.generate() will return the same Array
+     */
+    public int[] getGraphValues(){ return values; }
+
+    /**
+     *
+     * @return Returns the Array of generated Values
+     * The Graph can also directly be generated
+     * For later Versions, that can generate multiple Graphs at once, use the generate() method from the Generator
+     */
     public int[] generate() {
         values = new int[size + 1];
         Expression expression = new ExpressionBuilder(function).variable("x").build();
