0f7e7e0efdced1faeef1a9939a0a30dde9c07255
SlaxXxX <Janis.Schneider97@web.de>
2018-01-21 15:22:50 +0100

    1.2.0: Back to jdk 1.8

---
+++ src/test/java/de/slxSoft/graphGen/SettingsTest.java
@@ -0,0 +1,44 @@
+package de.slxSoft.graphGen;
+
+public class SettingsTest {
+
+    public static void main(String[] args) {
+        new SettingsTest();
+    }
+
+    private SettingsTest() {
+        Generator gen1 = new Generator();
+
+        Graph[] grap1 = gen1.addGraphs(3);
+        grap1[1].setSize(5);
+        grap1[1].setFunction("100+x");
+        grap1[1].setRelativeToLast(false);
+        grap1[1].setMaxOffset(0.8);
+        grap1[2].setSize(3);
+        grap1[2].setSubdecimals(3);
+        grap1[2].setChangeProbability(3);
+        grap1[2].setCooldownSpeed(50);
+
+        Generator gen2 = new Generator(gen1.getSettings());
+
+        System.out.println(gen1.getSettings());
+        System.out.println(gen2.getSettings());
+        System.out.println();
+        double[][] val1 = gen1.generate();
+        double[][] val2 = gen2.generate();
+
+        for (int i = 0; i < val1.length; i++) {
+            for (int j = 0; j < val1[i].length; j++)
+                System.out.print(val1[i][j] + ", ");
+            System.out.println();
+        }
+
+        System.out.println();
+
+        for (int i = 0; i < val2.length; i++) {
+            for (int j = 0; j < val2[i].length; j++)
+                System.out.print(val2[i][j] + ", ");
+            System.out.println();
+        }
+    }
+}
