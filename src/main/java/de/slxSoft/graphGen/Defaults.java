4d2dfacf70c7448927e5ed494ccacdb3e0f351da
SlaxXxX <Janis.Schneider97@web.de>
2020-06-17 20:22:29 +0200

    Update Defaults.java

--- src/main/java/de/slxSoft/graphGen/Defaults.java
+++ src/main/java/de/slxSoft/graphGen/Defaults.java
@@ -5,7 +5,7 @@ public class Defaults {
     public static boolean relativeToLast = true;
     public static double maxUpperOffset = 0.4;
     public static double maxLowerOffset = 0.4;
-    public static int changeProbability = 0;
+    public static int changeProbability = 1;
     public static int size = 100;
     public static int cooldownSpeed = 20;
     public static int subdecimals = 0;
